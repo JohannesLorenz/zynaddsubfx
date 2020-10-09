1728593e9a54a4a077aebf4cba12baa39d95c42f
Johannes Lorenz <j.git@lorenz-ho.me>
2020-10-09 18:48:58 +0200

    Port checker: Use longer timeout
    Required because MiddleWare can't reply when it calculates the wave
    table.

--- src/Tests/check-ports.rb
+++ src/Tests/check-ports.rb
@@ -8,7 +8,11 @@ Open3.popen3(Dir.pwd + "/../zynaddsubfx -O null --no-gui") do |stdin, stdout, st
   while line=stderr.gets do 
     # print "line: " + line;
     if /^lo server running on (\d+)$/.match(line) then
-      rval = system(Dir.pwd + "/../../rtosc/port-checker 'osc.udp://localhost:" + $1 + "/'")
+      # Timeout 500 is currently required because MiddleWare can be too busy to reply when it
+      # calls OscilGen::get thousands of times in a row to calculate the whole wave table at once.
+      # This should be changed once MiddleWare will calculate the wave table bit by bit and will
+      # be more responsive.
+      rval = system(Dir.pwd + "/../../rtosc/port-checker --timeout 500 'osc.udp://localhost:" + $1 + "/'")
       Process.kill("KILL", pid)
       exit rval
     end
