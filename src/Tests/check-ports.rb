9f92564ebfe5877eb42b68b1576df1a716308a87
Johannes Lorenz <j.git@lorenz-ho.me>
2020-10-30 14:21:25 +0100

    check-ports.rb: Detect zyn crash

--- src/Tests/check-ports.rb
+++ src/Tests/check-ports.rb
@@ -2,6 +2,8 @@
 
 require 'open3'
 
+rval=0
+
 # start zyn, grep the lo server port, and connect the port checker to it
 Open3.popen3(Dir.pwd + "/../zynaddsubfx -O null --no-gui") do |stdin, stdout, stderr, wait_thr|
   pid = wait_thr[:pid]
@@ -12,9 +14,28 @@ Open3.popen3(Dir.pwd + "/../zynaddsubfx -O null --no-gui") do |stdin, stdout, st
       # calls OscilGen::get thousands of times in a row to calculate the whole wave table at once.
       # This should be changed once MiddleWare will calculate the wave table bit by bit and will
       # be more responsive.
-      rval = system(Dir.pwd + "/../../rtosc/port-checker --timeout 500 'osc.udp://localhost:" + $1 + "/'")
-      Process.kill("KILL", pid)
-      exit rval
+      port_checker_rval = system(Dir.pwd + "/../../rtosc/port-checker --timeout 500 'osc.udp://localhost:" + $1 + "/'")
+      if port_checker_rval != true then
+        puts "Error: port-checker has returned #{$?.exitstatus}."
+        rval=1
+      end
+      begin
+        # Check if zyn has not crashed yet
+        # Wait 1 second before detection
+        sleep 1
+        Process.kill(0, pid)
+        # This is only executed if Process.kill did not raise an exception
+        Process.kill("KILL", pid)
+      rescue Errno::ESRCH
+        puts "Error: ZynAddSubFX (PID #{pid}) crashed!"
+        puts "       Missing replies or port-checker crashes could be due to the crash."
+        rval=1
+      rescue
+        puts "Error: Cannot kill ZynAddSubFX (PID #{pid})?"
+        rval=1
+      end
     end
   end
 end
+
+exit rval
